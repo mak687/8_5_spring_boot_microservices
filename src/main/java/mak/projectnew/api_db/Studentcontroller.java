5bba2cc91e414038969e91fbf5c4f9634d6df693
mak687 <mak687@hotmail.com>
2024-12-05 14:10:29 +0500

    Change Method Name

--- src/main/java/mak/projectnew/api_db/Studentcontroller.java
+++ src/main/java/mak/projectnew/api_db/Studentcontroller.java
@@ -3,6 +3,7 @@ package mak.projectnew.api_db;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,19 +18,29 @@ public class Studentcontroller {
     StudentRepo repo;
     
     @GetMapping("")
-    public Collection<Student> getall(){
+    public Collection<Student> getAllStudent(){
         return repo.findAll();
     }
 
     @PostMapping("/add")
-    public String postMethodName(@RequestBody Student student){
+    public String addStudent(@RequestBody Student student){
         repo.save(student);
         return "Student Saved as" + student.getName();
     }
 
     @PostMapping("/addmany")
-    public String pdMethodName(@RequestBody Collection<Student> students){
+    public String addManyStudent(@RequestBody Collection<Student> students){
         repo.saveAll(students);
         return students.size()+" Student Saved ";
+    }
+
+    @DeleteMapping("/delete")
+   public String deleteStudent(@RequestBody Student student) {
+  
+        if (student != null) {
+            repo.delete(student);
+            return  " Deleted Successfulyy.";
+        }
+        return null; 
     }
 }
